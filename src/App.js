16425a3dd153791cf2eda1b2c954e252d2843ca3
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2022-05-04 01:57:04 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const suggestions = ["React", "HTML", "CSS"];
+	const [isFocus, setIsFocus] = useState(false);
+	const [isHovered, setIsHovered] = useState(false);
+	const inputRef = useRef();
+	const [inputValue, setInputValue] = useState("");
+
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<div>
+				<h1 className="text-4xl font-bold">React Input Auto Suggestion</h1>
+				<div className="mt-10 relative">
+					<input
+						className="w-full focus:outline-none border-2 p-5"
+						placeholder="focus to get suggestion"
+						onFocus={() => setIsFocus(true)}
+						onBlur={() => {
+							if (!isHovered) {
+								setIsFocus(false);
+							}
+						}}
+						value={inputValue}
+						onChange={(e) => {
+							setInputValue(e.target.value);
+						}}
+						ref={inputRef}
+					/>
+					{isFocus && (
+						<div
+							className="shadow-lg absolute w-full"
+							onMouseEnter={() => {
+								setIsHovered(true);
+							}}
+							onMouseLeave={() => {
+								setIsHovered(false);
+							}}
+						>
+							{suggestions.map((suggestion, index) => {
+								const isMatch =
+									suggestion.toLowerCase().indexOf(inputValue.toLowerCase()) >
+									-1;
+								return (
+									<div key={index}>
+										{isMatch && (
+											<div
+												className="p-5 hover:bg-gray-200 cursor-pointer"
+												onClick={() => {
+													setInputValue(suggestion);
+													inputRef.current.focus();
+												}}
+											>
+												{suggestion}
+											</div>
+										)}
+									</div>
+								);
+							})}
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
-
-export default App;
